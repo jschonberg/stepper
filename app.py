eb5d6d5514d634ab50bbad421fdf14cdcbc70e59
Jacob Schonberg <jacob.schonberg@gmail.com>
2012-02-21 22:22:41 -0700

    heroku

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-def get_resource_as_string(name, charset='utf-8'):
-    with app.open_resource(name) as f:
-        return f.read().decode(charset)
-
-app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
-
-@app.route('/')
+@app.route('/index.html')
 def hello():
     openfile = open('./index.html', 'r')
     file_text = openfile.read()
