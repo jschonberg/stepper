46b0d5ebb474abae34355e4379175c2440e6f66e
Jacob Schonberg <jacob.schonberg@gmail.com>
2012-02-23 00:19:55 -0700

    heroku

--- static/script/script.js
+++ static/script/script.js
@@ -415,7 +415,7 @@ function changeNumberColumns(new_columns_object){
         var meter_object = $('.meterContainer').slice(total_beats,new_beats);
         $('.barContainer').slice(total_beats,new_beats)
                           .add(note_object)
-                          .add(meter_object).fadeIn('fast').animate(opacity:1);
+                          .add(meter_object).fadeIn('fast').animate({opacity:1});
     }    
     
     //Change Which item is bolded
